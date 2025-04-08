92253ed38b7cbb0a99808adfff40d7ee667870ad
Vi Ly <vi.lh@>
2025-04-08 09:36:52 +0700

    - add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 import { Providers } from './providers';
 
